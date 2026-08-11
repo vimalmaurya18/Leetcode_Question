971790641253fb809a739f690d59ebff6a277f35
vimalmaurya18 <vm5800034@gmail.com>
2026-08-11 20:17:30 +0530

    Time: 0 ms (100.00%) | Memory: 26.4 MB (6.99%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -2,21 +2,47 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int s=0,e=nums.size()-1,mid=s+((e-s)/2);
+        if(nums.size()==1)
+        {
+           return nums[0];
+        }
+        if(nums[0]!=nums[1])
+        {
+            return nums[0];
+        }
+        if(nums[nums.size()-1]!=nums[nums.size()-2])
+        {
+            return nums[nums.size()-1];
+        }
         while(s<e)
         {
+            if(mid >0 && mid<nums.size()-1 && nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])
+            {
+                return nums[mid];
+            }
+            if(mid >0 && nums[mid]==nums[mid-1])
+            {
+                if((mid-s+1)%2==0)
+                {
+                    s=mid+1;
+                }
+                else
+                {
+                    e=mid-2;
+                }
+            }
+            else if(mid<nums.size()-1 && nums[mid]==nums[mid+1])
+            {
+                if((e-mid+1)%2==0)
+                {
+                    e=mid-1;
+                }
+                else
+                {
+                    s=mid+2;
+                }
+            }
             mid=s+((e-s)/2);
-            if(mid%2==1)
-            {
-                mid=mid-1;
-            }
-            if(nums[mid]==nums[mid+1])
-            {
-                s=mid+2;
-            }
-            else
-            {
-                e=mid;
-            }
         }
         return nums[s];
     }

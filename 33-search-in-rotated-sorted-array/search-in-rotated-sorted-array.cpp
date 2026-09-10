c066a7653eaeeea43f76c6eeb9146290c42b9a85
vimalmaurya18 <vm5800034@gmail.com>
2026-09-10 23:01:59 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (37.22%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,49 +1,84 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        //fristly we have to find the pivat element
-        int s=0,e=nums.size()-1,mid=((e-s)/2)+s;
-        int p;
-        while(s<e)
-        {
-           if(nums[mid]>=nums[0])
-            {
-              s=mid+1; 
-            }
-            else
-            {
-                e=mid;
-            }
-            mid=((e-s)/2)+s;
-        }
-        p=s;
-        if(target>=nums[p] && target<=nums[nums.size()-1])
-        {
-           s=p;
-           e=nums.size()-1;
-        }
-        else
-        {
-            e=p-1;
-            s=0;
-        }
-        mid=((e-s)/2)+s;
-       while(s<=e)
-       {
+    //     //fristly we have to find the pivat element
+    //     int s=0,e=nums.size()-1,mid=((e-s)/2)+s;
+    //     int p;
+    //     while(s<e)
+    //     {
+    //        if(nums[mid]>=nums[0])
+    //         {
+    //           s=mid+1; 
+    //         }
+    //         else
+    //         {
+    //             e=mid;
+    //         }
+    //         mid=((e-s)/2)+s;
+    //     }
+    //     p=s;
+    //     if(target>=nums[p] && target<=nums[nums.size()-1])
+    //     {
+    //        s=p;
+    //        e=nums.size()-1;
+    //     }
+    //     else
+    //     {
+    //         e=p-1;
+    //         s=0;
+    //     }
+    //     mid=((e-s)/2)+s;
+    //    while(s<=e)
+    //    {
+    //     if(nums[mid]==target)
+    //     {
+    //         return mid;
+    //     }
+    //     else if(nums[mid]<target)
+    //     {
+    //         s=mid+1;
+    //     }
+    //     else
+    //     {
+    //         e=mid-1;
+    //     }
+    //     mid=((e-s)/2)+s;
+    //    }
+
+
+    // method second---> striver
+    int s=0,e=nums.size()-1;
+    while(s<=e)
+    {
+        int mid=((e-s)/2)+s;
         if(nums[mid]==target)
         {
             return mid;
         }
-        else if(nums[mid]<target)
+        //when right hand sorted
+        if(nums[mid]<=nums[e])
         {
-            s=mid+1;
+            if(nums[mid]<=target && target<=nums[e])
+            {
+                s=mid+1;
+            }
+            else
+            {
+                e=mid-1;
+            }
         }
         else
         {
-            e=mid-1;
+            if(nums[mid]>=target && target>=nums[s])
+            {
+                e=mid-1;
+            }
+            else
+            {
+                s=mid+1;
+            }
         }
-        mid=((e-s)/2)+s;
-       }
+    }
         return -1;
     }
 };
